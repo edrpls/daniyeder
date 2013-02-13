69784e9a95fad145f6b3cfd170c05e27c4fa52c4
Eder <eder@brutalcontent.com>
2013-02-12 18:11:27 -0600

    Fistr

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://gemcutter.org'
+
+gem 'rails', '3.2.11'
+gem 'mysql2'
+gem 'slim' # view engine
+gem 'omniauth' #social services
+gem 'omniauth-facebook' #social services
+gem 'omniauth-twitter' #social services
+gem 'activeadmin'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+  gem 'thin' #web server local
+  gem 'bullet'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daniyeder::Application.config.secret_token = '6dabbd45c57d7f5510669526f1c418249c01ca9f42146d41650aa716aecc40c316b115bf40d65cac869b99304992e4f5a60e7f0f83fa16ee1804f36d35d765eb'
